a752e7573c0a6add8d08ffb28db967ace4cd375a
Nikita <blubyuksetgje4gmail.com>
2024-01-08 16:56:21 +0600

    sixsteen

--- mynews/settings.py
+++ mynews/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+e8j8+l173t($c(fiatd+v8by$oj9_=al!blm%30trfac@(4%c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
